8c4818ab5946b35f386f45c13ae301dfb539ff24
meledych1111-ux <meledych1111@gmail.com>
2025-12-06 18:11:25 +0300

    Delete bot/init.py

--- bot/init.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher
-import os
-from .db.models import init_db
-
-bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode="HTML")
-dp = Dispatcher()
-
-init_db()
-from .handlers import *
